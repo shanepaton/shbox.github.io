ee76df3ea4568484f5c5314998c25456bd32838c
Shane_Games <45543346+Shane-Games@users.noreply.github.com>
2019-04-16 21:59:38 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+let shanesData = {
+    firstName: "Shane",
+    lastName: "Paton",
+    age: 11.0416,
+    favoriteColours: ["Red", "Blue"],
+    isOfAge: true,
+    canCode: null
+}
+
+document.getElementById("eventClick").onclick = function(){alert("Alert Works!"), eventClick.value="lert"}
