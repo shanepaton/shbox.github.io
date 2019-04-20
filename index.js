d627d15789eb4e0543c1bac4a551f8c41b6aac59
Shane_Games <45543346+Shane-Games@users.noreply.github.com>
2019-04-20 18:32:34 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ function playSound() {
     errorSound.play();
 }
 playSound();
-var numOfClicks = 0;
 alert("Welcome to Shane-Games");
+var numOfClicks = 0;
 document.getElementById("eventClick").onclick = function() {
     numOfClicks += 1;
     eventClick.value = "Clicked "+ numOfClicks; 
