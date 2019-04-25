84723d5b2a26673f64b7fc9414c3b5f27c1f0180
Shane_Games <45543346+Shane-Games@users.noreply.github.com>
2019-04-24 20:33:10 -0400

    f

---
+++ js/index.js
@@ -0,0 +1,12 @@
+var errorSound = document.getElementById("alertError")
+
+function playSound() {
+    errorSound.play();
+}
+playSound();
+alert("Welcome to Shane-Games");
+var numOfClicks = 0;
+document.getElementById("eventClick").onclick = function() {
+    numOfClicks += 1;
+    eventClick.value = "Clicked " + numOfClicks;
+}
