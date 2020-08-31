f8ee6eeb8a0cce58e57cd19a841192aa5e291b75
Shane_Games <45543346+Shane-Games@users.noreply.github.com>
2020-08-31 15:54:12 -0400

    haha secret go brrrrrrrr

--- js/secret.js
+++ js/secret.js
@@ -1,6 +1,18 @@
 let clicks = 0;
 
 function updateButton(){
+    if(clicks >= 99){
+        document.getElementById('button').style.backgroundColor = '#94e689'
+    }
+
+    if(clicks >= 999){
+        document.getElementById('button').style.backgroundColor = '#85b4ff'
+    }
+    
+    if(clicks == Infinity){
+        document.getElementById('button').style.backgroundColor = '#fa61ff'
+    }
+
     clicks++;
     document.getElementById('button').innerHTML = 'Clicked: ' + clicks;
 }
